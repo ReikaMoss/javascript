048dfc71b6eb6855221a287541ab0001eb37765c
Laura <laura.unam.ents@gmail.com>
2018-05-18 02:35:10 -0500

    commit inicial

---
+++ lecciones-variables/app.js
@@ -0,0 +1,4 @@
+var age = prompt ("¿Cual es tu edad?");
+var ageInseconds = age*365*24*60*60;
+console.log(ageInseconds);
+document.write("Tu edad es" + age + " y en segundos seria "  + ageInseconds);
---
+++ reto/app.js
@@ -0,0 +1,17 @@
+var name = prompt("¿Cuál es tu nombre?");
+document.write ("Bienvenida " + name);
+var question = prompt("¿Deseas jugar?");
+//Agregar SI O NO ¿?//
+
+//Si la respuesta es Si, se hace preguntas//
+var question = [
+  "¿Quién es el presidente de México?",
+  "¿Cual es el mejor amigo del hombre?",
+ "¿Cual es la esperanza de vida en México?"
+];
+
+  var answer = [
+  ["El bronco", "Lupita Dalesio", "Enrique Peña Nieto"],
+  ["gato", "pajaro", "elefante"],
+  ["75","70","80"]
+];
